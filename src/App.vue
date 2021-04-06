43388fb9d3e6233c070b094b01f26455845bb6ba
miko-github <mikoloism.github@gmail.com>
2021-04-06 18:20:54 +0430

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+    <b-tabs fill> </b-tabs>
     <router-view />
   </div>
 </template>
