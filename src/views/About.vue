8bb10bdf3bd255d563a599f34a97d3d44f04e4cd
miko-github <mikoloism.github@gmail.com>
2021-04-08 03:26:15 +0430

    change default vue page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <article class="about">
+    <header>
+      <h1>This is an about page</h1>
+      <span class="text-muted">
+        powerd by #(miko) &copy; {{ new Date().getFullYear() }}
+        <b-link class="mr-1 ml-1" href="https://github.com/miko-github">
+          <b-icon-github />
+        </b-link>
+        <b-link class="mr-1 ml-1" href="mailto:mikoloism.github@gmail.com">
+          <b-icon-envelope-fill />
+        </b-link>
+        <b-link class="mr-1 ml-1" href="https://instagram.com/mikoloism.pro">
+          <b-icon-instagram />
+        </b-link>
+      </span>
+    </header>
+  </article>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div id="nav">
+      <b-nav fill align="center">
+        <b-nav-item>
+          <div
+            class="d-flex flex-column justify-content-between align-items-center align-content-between"
+          >
+            <b-icon-house />
+            <router-link to="/">Home</router-link>
+          </div>
+        </b-nav-item>
+        <b-nav-item>
+          <div
+            class="d-flex flex-column justify-content-between align-items-center align-content-between"
+          >
+            <b-icon-archive />
+            <router-link to="/about">About</router-link>
+          </div>
+        </b-nav-item>
+        <b-nav-item>
+          <div
+            class="d-flex flex-column justify-content-between align-items-center align-content-between"
+          >
+            <b-icon-layout-text-window />
+            <router-link to="/dashboard">Dashboard</router-link>
+          </div>
+        </b-nav-item>
+      </b-nav>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
-  },
+  components: {},
 };
 </script>
