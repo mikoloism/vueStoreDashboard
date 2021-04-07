f4fb7d0fc7a8a1d5271b93ed645dd31aadf09580
miko-github <mikoloism.github@gmail.com>
2021-04-08 03:28:50 +0430

    initial commit

---
+++ src/views/tabs-view/price-view.vue
@@ -0,0 +1,27 @@
+<template>
+  <b-form-group label-for="v-general__product--unit">
+    <b-form-input
+      type="text"
+      id="v-general__product--unit"
+      v-model="product.unit"
+      list="v-general__product--unit-list"
+      placeholder="واحد شمارش"
+      required
+    />
+    <template #description>
+      <p>اگر واحد شمارشی مورد نظر یافت نشد، آن را بنویسید</p>
+    </template>
+    <b-datalist :options="builts.units" id="v-general__product--unit-list" />
+  </b-form-group>
+</template>
+
+<script>
+import builts from "@/store/built.js";
+export default {
+  data() {
+    return { builts };
+  },
+};
+</script>
+
+<style></style>
