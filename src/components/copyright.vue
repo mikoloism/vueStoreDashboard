85fd90330dd0f3e2e9a087017c5f5c02ae389db0
miko-github <mikoloism.github@gmail.com>
2021-04-09 03:04:43 +0430

    exporting copyright (no i dont love it, but love open source with name) to another component

---
+++ src/components/copyright.vue
@@ -0,0 +1,14 @@
+<template>
+  <span class="text-muted">
+    powerd by #(miko) &copy; {{ new Date().getFullYear() }}
+    <b-link class="mr-1 ml-1" href="https://github.com/miko-github">
+      <b-icon-github />
+    </b-link>
+    <b-link class="mr-1 ml-1" href="mailto:mikoloism.github@gmail.com">
+      <b-icon-envelope-fill />
+    </b-link>
+    <b-link class="mr-1 ml-1" href="https://instagram.com/mikoloism.pro">
+      <b-icon-instagram />
+    </b-link>
+  </span>
+</template>
--- src/views/About.vue
+++ src/views/About.vue
@@ -2,18 +2,13 @@
   <article class="about">
     <header>
       <h1>This is an about page</h1>
-      <span class="text-muted">
-        powerd by #(miko) &copy; {{ new Date().getFullYear() }}
-        <b-link class="mr-1 ml-1" href="https://github.com/miko-github">
-          <b-icon-github />
-        </b-link>
-        <b-link class="mr-1 ml-1" href="mailto:mikoloism.github@gmail.com">
-          <b-icon-envelope-fill />
-        </b-link>
-        <b-link class="mr-1 ml-1" href="https://instagram.com/mikoloism.pro">
-          <b-icon-instagram />
-        </b-link>
-      </span>
+      <v-copy />
     </header>
   </article>
 </template>
+<script>
+import VCopy from "@/components/copyright";
+export default {
+  components: { VCopy },
+};
+</script>
