0d9f5bb93b9632d77b3ffac2983d40e7bb8808ef
miko-github <mikoloism.github@gmail.com>
2021-04-10 02:49:55 +0430

    Change some props 👍

--- src/components/pictures/v-image-box.vue
+++ src/components/pictures/v-image-box.vue
@@ -1,17 +1,18 @@
 <template>
   <b-row>
     <b-col
-      v-for="{ src, file, index } in this.files"
+      v-for="{ src, realName, name, index } in this.files"
       :key="index + 1"
       md="6"
       sm="6"
       class="mb-2 mt-2 v-picture__preview-wrap"
+      cols="6"
     >
       <b-btn-group class="v-picture__preview-btn">
         <b-btn
           class="v-picture__delete-icon"
           :data-index="index + 1"
-          @dblclick="handleRemoveSelected"
+          @dblclick="handleRemove"
           variant="outline-danger"
         >
           <b-icon icon="trash-fill" font-scale="1.5" />
@@ -19,14 +20,14 @@
         <b-btn
           class="v-picture__edit-icon"
           :data-index="index + 1"
-          @click="handleSetName"
+          @click="handleRename"
           variant="outline-info"
         >
           <b-icon icon="pencil-square" font-scale="1.5" />
         </b-btn>
       </b-btn-group>
-      <v-img :src="src" :index="index" :name="file.name" />
-      <p>{{ file.name }}</p>
+      <v-img :src="src" :index="index" :name="name" :real="realName" />
+      <p>{{ name }}</p>
     </b-col>
   </b-row>
 </template>
@@ -37,7 +38,10 @@ export default {
   name: "VImageBox",
   props: ["files"],
   components: { VImg },
+
+  methods: {
+    handleRename() {},
+    handleRemove() {},
+  },
 };
 </script>
-
-<style></style>
