62efc3fa652e7f47cc208dc30ac069e28f2755cb
miko-github <mikoloism.github@gmail.com>
2021-04-14 19:59:40 +0430

    update price-view.vue
    added form-group compoents
        v-sell-state, v-unit-select, v-quantity v-exemption

--- src/views/tabs-view/price-view.vue
+++ src/views/tabs-view/price-view.vue
@@ -5,9 +5,13 @@
     </b-col>
     <b-col cols="11" class="text-right">
       <b-form dir="rtl" @submit="onSubmit">
-        <v-sell-state />
-        <v-unit-select @change="(value) => (this.product.unit = value)" />
-        <v-quantity />
+        <v-sell-state @change="(v) => (this.product.states = v)" />
+        <v-unit-select @change="(v) => (this.product.unit = v)" />
+        <v-quantity
+          @min="(n) => (this.product.quantity.min = n)"
+          @max="(x) => (this.product.quantity.max = x)"
+        />
+        <v-exemption @change="(v) => (this.product.exemption = v)" />
         <v-tabs-nav />
         {{ this.product.unit }}
       </b-form>
@@ -20,13 +24,19 @@ import builts from "@/store/built.js";
 import vTabsNav from "@/components/tabs/v-tabs-nav.vue";
 import VUnitSelect from "@/components/pricing/v-unit-select.vue";
 import VSellState from "@/components/pricing/v-sell-state.vue";
-import VQuantity from "../../components/pricing/v-quantity.vue";
+import VQuantity from "@/components/pricing/v-quantity.vue";
+import VExemption from "@/components/pricing/v-exemption.vue";
 export default {
-  components: { vTabsNav, VUnitSelect, VSellState, VQuantity },
+  components: { vTabsNav, VUnitSelect, VSellState, VQuantity, VExemption },
   data() {
     return {
       builts,
-      product: { unit: "", sellState: false, productState: false },
+      product: {
+        unit: null,
+        states: {},
+        quantity: { max: 0, min: 0 },
+        exemption: "customer ",
+      },
     };
   },
   methods: {
