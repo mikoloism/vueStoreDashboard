1982ac6cd70d867546001d849c11e21d127884db
miko-github <mikoloism.github@gmail.com>
2021-04-14 19:56:03 +0430

    add v-exmption.vue helper => (customer=true def, seller=false|null)

---
+++ src/components/pricing/v-exemption.vue
@@ -0,0 +1,39 @@
+<template>
+  <b-form-group>
+    <!-- customer=true | seller=false -->
+    <label>
+      معافیت ارزش افزوده برای :
+      <b-form-checkbox
+        class="d-inline m-2"
+        button
+        :button-variant="value ? 'primary' : 'outline-primary'"
+        v-model="value"
+        size="sm"
+      >
+        {{ value ? "خریدار" : "فروشنده" }}
+      </b-form-checkbox></label
+    >
+  </b-form-group>
+</template>
+
+<script>
+export default {
+  data() {
+    return { value: true };
+  },
+  watch: {
+    value(vn, vp) {
+      if (vn !== vp) {
+        if (vn) {
+          this.trigger("change", vn);
+        }
+      }
+    },
+  },
+  methods: {
+    trigger(emit, arg) {
+      this.$emit(emit, arg ? "customer" : "seller");
+    },
+  },
+};
+</script>
