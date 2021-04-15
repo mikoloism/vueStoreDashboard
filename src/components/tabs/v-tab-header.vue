1e01d88f38f66c6ba00571b8429c5e223b123612
miko-github <mikoloism.github@gmail.com>
2021-04-15 05:27:39 +0430

    create v-tab-header.vue
    this component go to be as header and navigate bar on the each tab-view

---
+++ src/components/tabs/v-tab-header.vue
@@ -0,0 +1,24 @@
+<template>
+  <b-row class="text-right" align-h="center">
+    <b-col cols="3">
+      <b-row>
+        <b-col cols="6">
+          <b-icon icon="chevron-double-left" />
+        </b-col>
+        <b-col cols="6">
+          <b-btn size="sm" variant="link">
+            پیشین
+            <b-icon icon="arrow-right" />
+          </b-btn>
+        </b-col>
+      </b-row>
+    </b-col>
+    <b-col class="h3 text-right pr-5 p-3" align-self="start" cols="6">
+      <h3>{{ header }}</h3>
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+export default { props: ["header"] };
+</script>
