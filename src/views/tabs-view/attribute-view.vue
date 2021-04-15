92fca4853490f4d592f91f045b4181b2171e803f
miko-github <mikoloism.github@gmail.com>
2021-04-15 21:31:51 +0430

    Update attribute-view.vue
    adding to attributes array

--- src/views/tabs-view/attribute-view.vue
+++ src/views/tabs-view/attribute-view.vue
@@ -2,7 +2,7 @@
   <b-row align-h="center">
     <b-col cols="11">
       <!-- <b-form> -->
-      <v-input-box />
+      <v-input-box @add="addAttribute" />
       <v-attribute-list />
       <!-- <v-tabs-nav /> -->
       <!-- </b-form> -->
@@ -14,6 +14,14 @@
 // import VTabsNav from "@/components/tabs/v-tabs-nav.vue";
 import VInputBox from "@/components/attributes/v-input-box.vue";
 export default {
+  data() {
+    return { attributes: [] };
+  },
+  methods: {
+    addAttribute({ name, value }) {
+      this.attributes.push({ name, value });
+    },
+  },
   components: { VInputBox },
 };
 </script>
