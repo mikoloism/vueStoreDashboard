d5f1db1fca6fd4c94a785ca610cf02609a64c838
miko-github <mikoloism.github@gmail.com>
2021-04-15 21:31:12 +0430

    Create v-input-box.vue (attributes-view)

--- src/components/attributes/v-input-box.vue
+++ src/components/attributes/v-input-box.vue
@@ -1,17 +1,48 @@
 <template>
   <b-input-group>
-    <b-form-input placeholder="نام ویژگی (لاتین)" />
-    <b-form-input placeholder="مقدار ویژگی (لاتین)" />
-    <template slot="append">
-      <b-btn variant="outline-primary">
-        <b-icon icon="plus" scale="2" />
+    <b-form-input
+      v-model="attribute.name"
+      :state="state.name"
+      dir="rtl"
+      placeholder="مقدار ویژگی"
+      tabindex="1"
+    />
+    <b-form-input
+      v-model.trim="attribute.value"
+      :state="state.value"
+      dir="rtl"
+      placeholder="نام ویژگی"
+      tabindex="0"
+    />
+    <template slot="prepend">
+      <b-btn
+        @click="validate"
+        variant="outline-primary"
+        class="d-flex align-items-center"
+        tabindex="2"
+      >
+        <b-icon icon="plus" scale="1.7" class="p-0" />
       </b-btn>
     </template>
   </b-input-group>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      attribute: { name: "", value: "" },
+      state: { name: null, value: null },
+    };
+  },
+  methods: {
+    validate() {
+      this.state.name = this.attribute.name.length !== 0;
+      this.state.value = this.attribute.value.length !== 0;
+      if (this.state.name && this.state.value) this.$emit("add", this.state);
+    },
+  },
+};
 </script>
 
 <style></style>
