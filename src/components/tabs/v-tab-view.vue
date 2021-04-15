9ae5a2641c7948a9db5a5ebce50507199d086814
miko-github <mikoloism.github@gmail.com>
2021-04-15 06:20:08 +0430

    update v-tab-view.vue
    added styles

--- src/components/tabs/v-tab-view.vue
+++ src/components/tabs/v-tab-view.vue
@@ -1,8 +1,12 @@
 <template>
-  <div>
-    <v-tab-header :header="header" />
-    <component :is="body" />
-  </div>
+  <b-row align-h="center">
+    <div class="col-12">
+      <v-tab-header class="overflow-hidden" :header="header" />
+    </div>
+    <div class="p-3 col-12">
+      <component :is="body" />
+    </div>
+  </b-row>
 </template>
 <script>
 import vTabHeader from "./v-tab-header.vue";
