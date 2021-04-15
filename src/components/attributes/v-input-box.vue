22fc61235b86189a2397d6c16b44f2ab3cd6409b
miko-github <mikoloism.github@gmail.com>
2021-04-15 05:02:45 +0430

    create v-input-box.vue (attributes)
    two input box with one button to submiting new attribute to list

---
+++ src/components/attributes/v-input-box.vue
@@ -0,0 +1,17 @@
+<template>
+  <b-input-group>
+    <b-form-input placeholder="نام ویژگی (لاتین)" />
+    <b-form-input placeholder="مقدار ویژگی (لاتین)" />
+    <template slot="append">
+      <b-btn variant="outline-primary">
+        <b-icon icon="plus" scale="2" />
+      </b-btn>
+    </template>
+  </b-input-group>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
