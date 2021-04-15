b86e7ab71777c56b562b4c6602b48ec19b2fcf02
miko-github <mikoloism.github@gmail.com>
2021-04-15 06:18:51 +0430

    fixing box-sizing in bootstrap

--- src/components/tabs/v-tab-header.vue
+++ src/components/tabs/v-tab-header.vue
@@ -1,22 +1,25 @@
 <template>
-  <b-row class="text-right" align-h="center">
-    <b-col cols="3">
-      <b-row>
-        <b-col cols="6">
+  <div
+    dir="ltr"
+    class="d-flex overflow-hidden p-2 pb-1 pt-4 border border-1 text-right"
+  >
+    <div class="col-4">
+      <div class="row">
+        <b-btn class="col-6" size="sm" variant="link">
           <b-icon icon="chevron-double-left" />
-        </b-col>
-        <b-col cols="6">
-          <b-btn size="sm" variant="link">
-            پیشین
-            <b-icon icon="arrow-right" />
-          </b-btn>
-        </b-col>
-      </b-row>
-    </b-col>
-    <b-col class="h3 text-right pr-5 p-3" align-self="start" cols="6">
-      <h3>{{ header }}</h3>
-    </b-col>
-  </b-row>
+          بعدی
+        </b-btn>
+        <b-btn class="col-6" size="sm" variant="link">
+          بازگشت
+          <b-icon icon="arrow-right" />
+        </b-btn>
+      </div>
+    </div>
+
+    <div class="h3 text-right col-6">
+      <h3 class="cols-12">{{ header }}</h3>
+    </div>
+  </div>
 </template>
 
 <script>
