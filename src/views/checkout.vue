231ed1b118d5ea025a6fc23c01b043578435c220
miko-github <mikoloism.github@gmail.com>
2021-04-17 02:03:06 +0430

    create last view and checkout view

---
+++ src/views/checkout.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <header>
+      <!-- <h2 class="order-1">{{ product.name }}</h2> -->
+      <!-- <b-carousel class="order-0"></b-carousel> -->
+    </header>
+    <section>
+      <!-- <product-name /> -->
+
+      <!-- <description /> -->
+    </section>
+    <footer>
+      <!-- <copy /> -->
+    </footer>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/views/upload.vue
@@ -0,0 +1,19 @@
+<template>
+  <section>
+    <header>
+      <!-- <h3>{{ what is to upload }}</h3> -->
+    </header>
+    <section>
+      <!-- <b-progress-bar></b-progress-bar> -->
+    </section>
+  </section>
+  <!-- list => <which-one-uploaded /> -->
+  <!-- if successfull => <upload-success-modal /> -->
+  <!-- or, error -->
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
