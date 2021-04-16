c4935cd049fa5bbd0f01aafbfa09b6c9d05e8c47
miko-github <mikoloism.github@gmail.com>
2021-04-16 19:42:04 +0430

    update v-input.vue (attribute-view.vue) [work fine]

--- src/components/attributes/v-input-box.vue
+++ src/components/attributes/v-input-box.vue
@@ -1,25 +1,23 @@
 <template>
-  <b-input-group>
+  <b-input-group dir="rtl">
     <b-form-input
-      v-model="attribute.name"
+      v-model.trim="attribute.name"
       :state="state.name"
-      dir="rtl"
-      placeholder="مقدار ویژگی"
-      tabindex="1"
+      placeholder="نام ویژگی"
+      :value="attribute.name"
+      :readonly="editable ? true : false"
     />
     <b-form-input
-      v-model.trim="attribute.value"
+      v-model="attribute.value"
       :state="state.value"
-      dir="rtl"
-      placeholder="نام ویژگی"
-      tabindex="0"
+      placeholder="مقدار ویژگی"
+      :value="attribute.value"
     />
-    <template slot="prepend">
+    <template slot="append">
       <b-btn
         @click="validate"
         variant="outline-primary"
         class="d-flex align-items-center"
-        tabindex="2"
       >
         <b-icon icon="plus" scale="1.7" class="p-0" />
       </b-btn>
@@ -29,17 +27,31 @@
 
 <script>
 export default {
+  props: ["editable", "lastId"],
   data() {
     return {
       attribute: { name: "", value: "" },
       state: { name: null, value: null },
     };
   },
+  watch: {
+    editable(n, o) {
+      if ((n, o)) {
+        if (n) {
+          this.attribute = { name: n.name, value: n.value };
+        }
+      }
+    },
+  },
   methods: {
     validate() {
       this.state.name = this.attribute.name.length !== 0;
       this.state.value = this.attribute.value.length !== 0;
-      if (this.state.name && this.state.value) this.$emit("add", this.state);
+      if (this.state.name && this.state.value) {
+        this.$emit("add", this.attribute);
+        this.attribute = { name: "", value: "" };
+        this.state = { name: null, value: null };
+      }
     },
   },
 };
