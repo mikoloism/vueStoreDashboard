8f376b3fb229ec7a9fcd943eb27f72dfcb908f82
miko-github <mikoloism.github@gmail.com>
2021-04-16 19:41:14 +0430

    Update Attributes-view.vue (Added Methods)

--- src/views/tabs-view/attribute-view.vue
+++ src/views/tabs-view/attribute-view.vue
@@ -1,28 +1,51 @@
 <template>
   <b-row align-h="center">
     <b-col cols="11">
-      <!-- <b-form> -->
-      <v-input-box @add="addAttribute" />
-      <v-attribute-list />
-      <!-- <v-tabs-nav /> -->
-      <!-- </b-form> -->
+      <v-input-box :id="id" :editable="editable" @add="addAttribute" />
+      <v-attribute-list
+        @edit="editAttribute"
+        @remove="removeAttribute"
+        :attributes="attributes"
+      />
     </b-col>
   </b-row>
 </template>
 
 <script>
-// import VTabsNav from "@/components/tabs/v-tabs-nav.vue";
 import VInputBox from "@/components/attributes/v-input-box.vue";
+import VAttributeList from "@/layouts/attribute/v-attribute-list";
 export default {
   data() {
-    return { attributes: [] };
+    return {
+      editable: null,
+      id: 0,
+      attributes: [],
+    };
   },
   methods: {
     addAttribute({ name, value }) {
-      this.attributes.push({ name, value });
+      let duplicate = this.attributes.findIndex((item) => item.name === name);
+      if (duplicate === -1)
+        this.attributes.push({ id: ++this.id, name, value });
+      else {
+        let forEdit = this.attributes.find(
+          (item) => item.id === this.editable.id && item
+        );
+        this.editable = null;
+        forEdit.value = value;
+      }
     },
+    removeAttribute(index) {
+      let attrs = this.attributes.filter(({ id }) => id !== index);
+      this.attributes = attrs;
+    },
+    editAttribute(index) {
+      let editable = this.attributes.filter((attr) => attr.id === index)[0];
+      this.editable = editable;
+    },
+    StoreSync() {},
   },
-  components: { VInputBox },
+  components: { VInputBox, VAttributeList },
 };
 </script>
 
